d0acd28924fb8840c4633da13649c0bf5c9d8f04
TCLee-tech <81542713+TCLee-tech@users.noreply.github.com>
2021-05-03 06:31:35 +0800

    JavaScript Assessment

--- JavaScriptAssessment/findmyhat.js
+++ JavaScriptAssessment/findmyhat.js
@@ -33,7 +33,7 @@ class Field
   this.locationY = 0;
  }  //End of constructor
 
- //call a static method directly from the Class Object
+ //call a Static method directly from the Class Object
  static generateField(fieldH, fieldW, percentage = 0.1) {
 
     const field = new Array(fieldH)
@@ -45,12 +45,16 @@ class Field
       for(let x = 0; x < fieldW; x++) {
         const prob = Math.random();    //return a value betwen 0 and 1
         //console.log(prob);
-
         field[y][x] = prob > percentage ? fieldCharacter : hole
       }
     }
     return field;
   } //End of generateField method
+
+//create instance of Field Class to initiate the Constructor, generating rows and columns from generateField method.
+//call runGame to run the game  
+const myField = new Field(Field.generateField(10,10,0.2));
+myField.runGame();
 
   runGame() {
       //other methods in the class
@@ -59,6 +63,29 @@ class Field
   
     this.print();       //print out the rows and columns
     this.getInput();    //get input from the user
+
+    let playing = true;
+    while (playing) {
+      this.print();
+      this.askQuestion();
+      if (!this.isInBounds()) {
+        console.log('Out of bounds instruction!');
+        playing = false;
+        break;
+      } else if (this.isHole()) {
+        console.log('Sorry, you fell down a hole!');
+        playing = false;
+        break;
+      } else if (this.isHat()) {
+        console.log('Congrats, you found your hat!');
+        playing = false;
+        break;
+      }
+      // Update the current location on the map
+      this.field[this.locationY][this.locationX] = pathCharacter;
+    }
+  }
+
  }
  
  setStart() {
@@ -80,6 +107,23 @@ print() {
  
  getInput() {
     const input = prompt('Which way? ').toUpperCase();
+    switch (answer) {
+      case 'U':
+        this.locationY -= 1;
+        break;
+      case 'D':
+        this.locationY += 1;
+        break;
+      case 'L':
+        this.locationX -= 1;
+        break;
+      case 'R':
+        this.locationX += 1;
+        break;
+      default:
+        console.log('Enter U, D, L or R.');
+        this.getInput();
+        break;
  }
 
     //check input - u, d, l, r, and others (to warn user if wrong key entered)
@@ -90,19 +134,40 @@ print() {
       x: 0,
       y: 0
     }
-  
+ 
+    where (pos.y >= 0 &&
+      pos.x >= 0 &&
+      pos.y < this.field.length &&
+      pos.x < this.field[0].length) {
+
     pos.x = Math.floor(Math.random() * this.field[0].length);   //randomise x position, x = 10
     pos.y = Math.floor(Math.random() * this.field[1].length);   //randomise y position, y = 10
+    }
   
     console.log(pos.x);
     console.log(pos.y);
    
     return pos;
   }
-  
+
+  isHat() {
+    return this.field[this.locationY][this.locationX] === hat;
+  }
+
+  isHole() {
+    return this.field[this.locationY][this.locationX] === hole;
+  }
+
+  print() {
+    const displayString = this.field.map(row => {
+        return row.join('');
+      }).join('\n');
+    console.log(displayString);
+  }
+
  
 
-}   //end of field class
+}   //end of Field Class
 
 
 //create an instance of Field Class and call generateField directly from Class name
